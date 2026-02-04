486d2848da891bbcdd08ee173d156aabb69e0964
tamaygz <forevor@gmail.com>
2026-02-04 01:52:33 +0100

    refactor: Simplify ColorfulDiagramExtras by removing custom DnD provider

--- src/main/java/com/tamaygz/colorfuldiag/diagram/ColorfulDiagramExtras.java
+++ src/main/java/com/tamaygz/colorfuldiag/diagram/ColorfulDiagramExtras.java
@@ -1,41 +1,19 @@
 package com.tamaygz.colorfuldiag.diagram;
 
-import com.intellij.diagram.DiagramBuilder;
-import com.intellij.diagram.DiagramDataModel;
-import com.intellij.diagram.DiagramNode;
 import com.intellij.diagram.extras.DiagramExtras;
 import com.intellij.diagram.extras.providers.DiagramDnDProvider;
-import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 /**
  * Extended diagram functionality for Colorful Diagrams.
- * Provides additional drag-and-drop capabilities for containers.
+ * Provides additional capabilities for the diagram editor.
  */
 public class ColorfulDiagramExtras<T> extends DiagramExtras<T> {
 
     @Override
     public @Nullable DiagramDnDProvider<T> getDnDProvider() {
-        return new ColorfulDnDProvider<>();
-    }
-
-    /**
-     * Custom drag-and-drop provider for handling table drops onto containers.
-     */
-    private static class ColorfulDnDProvider<T> implements DiagramDnDProvider<T> {
-
-        @Override
-        public boolean isAcceptedForDnD(@NotNull Object element) {
-            // Accept any diagram element for DnD
-            return true;
-        }
-
-        @Override
-        public @Nullable String getActionForDnD(@NotNull Object element) {
-            return "Move to Container";
-        }
+        // Return null to use default DnD behavior
+        // Custom DnD can be implemented when the API requirements are clear
+        return null;
     }
 }
